e865456a3a43be1351a31a752d066d6f29735f84
Li-yao Xia <lysxia@gmail.com>
2024-10-08 17:26:34 +0200

    Remove json_visitor, we use jsonm instead

--- server/lib/tests.ml
+++ server/lib/tests.ml
@@ -1,14 +1,4 @@
 let%expect_test _ =
-  let open Json_visitor in
-  let visitor = { empty_visitor with
-    visit_null = fun path _ -> print_endline (string_of_path path)
-  } in
-  visit_string visitor "[[], {\"k\": null}, null]";
-  [%expect {|
-    $.k[1]
-    $[2]|}]
-
-let%expect_test _ =
   let open Hacky_coma_parser in
   let open Rust_syntax in
   let coma = parse_coma_string "module M_qyi1 (* T *)\nmodule M_z__y\n" in
