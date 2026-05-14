30628efca9987a097ef2d02cf3b43c7d4eb0ae5a
Vincent Lafeychine <vincent.lafeychine@proton.me>
2026-05-14 18:24:53 +0200

    chore(nix): Update to latest Nix from Creusot repository

--- flake.nix
+++ flake.nix
@@ -1,72 +1,92 @@
 {
   inputs = {
     nixpkgs.follows = "creusot/nixpkgs";
-    flake-utils.follows = "creusot/flake-utils";
+    flake-parts.follows = "creusot/flake-parts";
 
-    creusot.url = "github:creusot-rs/creusot/v0.11.0";
+    creusot.url = "github:creusot-rs/creusot";
   };
 
-  outputs = {
-    self,
-    creusot,
-    flake-utils,
-    nixpkgs,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-      pkgsCreusot = creusot.lib.${system}.pkgs;
+  outputs =
+    inputs@{
+      creusot,
+      flake-parts,
+      nixpkgs,
+      self,
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ];
 
-      version = "0.2.594";
-    in {
-      packages = {
-        code = pkgs.buildNpmPackage {
-          inherit version;
+      perSystem =
+        {
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          version = pkgs.creusot.creusot.version;
+        in
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ creusot.overlays.default ];
+          };
 
-          pname = "creusot-ide";
-          src = ./.;
-          npmDepsHash = "sha256-7bODwzezOOXmS43pezizOQ+1qfDD/8y23NVlDRvtpJY=";
+          formatter = pkgs.nixfmt-tree;
 
-          buildInputs = with pkgs; [libsecret];
-          nativeBuildInputs = with pkgs; [pkg-config];
+          packages = {
+            code = pkgs.buildNpmPackage {
+              inherit version;
 
-          prePatch = ''
-            sed -i -e 's/"0.1.1"/"${version}"/g' package.json package-lock.json
-          '';
+              pname = "creusot-ide";
+              src = ./.;
+              npmDepsHash = "sha256-8tB57OR3vBLdYk+cXRMwk/tQSDBurgH4uYwzz/j1Ncs=";
 
-          installPhase = ''
-            mkdir $out
-            npx vsce package -o $out/creusot-ide.vsix
-          '';
+              buildInputs = with pkgs; [ libsecret ];
+              nativeBuildInputs = with pkgs; [ pkg-config ];
+
+              prePatch = ''
+                sed -i -e 's/"0.1.1"/"${version}"/g' package.json package-lock.json
+              '';
+
+              installPhase = ''
+                mkdir $out
+                npx vsce package -o $out/creusot-ide.vsix
+              '';
+            };
+
+            lsp = pkgs.ocamlPackages.buildDunePackage {
+              inherit version;
+
+              pname = "creusot-lsp";
+              src = ./.;
+
+              nativeBuildInputs = [ pkgs.ocamlPackages.menhir ];
+              buildInputs =
+                (with pkgs.creusot; [
+                  why3
+                  why3find
+                ])
+                ++ (with pkgs.ocamlPackages; [
+                  dune-build-info
+                  dune-site
+                  jsonm
+                  linol-lwt
+                  logs
+                  lwt
+                  menhirLib
+                  ppx_deriving
+                  ppx_expect
+                  ppx_yojson_conv
+                  terminal_size
+                  toml
+                  uri
+                  xmlm
+                ]);
+            };
+          };
         };
-
-        lsp = pkgs.ocamlPackages.buildDunePackage {
-          inherit version;
-
-          pname = "creusot-lsp";
-          src = ./.;
-
-          nativeBuildInputs = [pkgs.ocamlPackages.menhir];
-          buildInputs =
-            (with pkgsCreusot; [why3 why3find])
-            ++ (with pkgs.ocamlPackages; [
-              dune-build-info
-              dune-site
-              jsonm
-              linol-lwt
-              logs
-              lwt
-              menhirLib
-              ppx_deriving
-              ppx_expect
-              ppx_yojson_conv
-              terminal_size
-              toml
-              uri
-              xmlm
-            ]);
-        };
-      };
-
-      formatter = pkgs.alejandra;
-    });
+    };
 }
